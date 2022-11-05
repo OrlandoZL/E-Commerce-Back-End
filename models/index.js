2df13eab766e7c5ea1bcb36a14d1159a8222d024
Orlando <orlandozepeda20@gmail.com>
2022-11-05 00:17:52 -0700

    finshed routes

--- models/index.js
+++ models/index.js
@@ -18,7 +18,7 @@ Product.belongsToMany(Tag, {
   foreignKey: 'product_id',
 });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(product, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id'
 });
